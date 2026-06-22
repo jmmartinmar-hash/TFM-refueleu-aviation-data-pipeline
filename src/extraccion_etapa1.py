eb15f3b063713418cc186b177fd80659f94ea89e
jmmartinmar <jmmartinmar@gmail.com>
2026-06-22 21:54:56 +0200

    extraccion etp1

--- src/extraccion_etapa1.py
+++ src/extraccion_etapa1.py
@@ -15,9 +15,9 @@ logging.basicConfig(
     handlers=[logging.FileHandler(log, encoding="utf-8"), logging.StreamHandler()],
 )
 
-# =====================================================================
-# 2. PROCESO PRINCIPAL DE EXTRACCIÓN 
-# =====================================================================
+# ===============================================================================
+# 2. PROCESO PRINCIPAL DE EXTRACCIÓN DE VUELOS CON SALIDA EN AEROPUERTOS DE LA UE
+# ===============================================================================
 def extraccion_eu_salidas():
     logging.info("Iniciando Extracción: Descarga de vuelos de la UE desde OpenSky API...")
 
@@ -39,27 +39,30 @@ def extraccion_eu_salidas():
     # --- CARGA DE TODOS LOS AEROPUERTOS DE LA UE DESDE FICHERO aeropuertos_eu.csv ---
     aeropuertos = "aeropuertos_eu.csv"
     if not os.path.exists(aeropuertos):
+        #registro en el log del error  por falta de fichero de aeropuertos
         logging.error(f"Error Crítico: No existe '{aeropuertos}'. Ejecutar primero 'src/generate_airports.py'")
         return
 
     try:
         df_aeropuertos = pd.read_csv(aeropuertos, header=None)
         eu_aeropuertos = set(df_aeropuertos[0].dropna().astype(str).str.strip().tolist())
+        #registro en el log del número de aeropuertos cargados
         logging.info(f"Cargados {len(eu_aeropuertos)} aeropuertos oficiales de la UE.")
     except Exception as e:
+        #registro en el log del error crítico al procesar el fichero de aeropuertos
         logging.error(f"Error al procesar '{aeropuertos}': {e}")
         return
 
     # --- INICIALIZACIÓN DE API ---
     if usuario_osky and password_osky:
-        # CAMBIO CRÍTICO: Pasamos los parámetros nombrados explícitamente para OAuth2
+        # Paso los parámetros nombrados explícitamente para OAuth2
         api = OpenSkyApi(client_id=usuario_osky, client_secret=password_osky)
         logging.info("Conectado a OpenSky con credenciales...")
     else:
         api = OpenSkyApi()
         logging.warning("Conectado de forma anónima...")
 
-    # ---Descarga de Día anterior completo ---
+    # ---Descarga de vuelos del Día anterior completo ---
     hoy = datetime.now(timezone.utc)
     ayer = hoy - timedelta(days=1)
     dia_ini = datetime(ayer.year, ayer.month, ayer.day, 0, 0, 0, tzinfo=timezone.utc)
@@ -80,14 +83,14 @@ def extraccion_eu_salidas():
         try:
             # Descarga masiva de todos los vuelos registrados en el mundo en el intervalo de tiempo especificado
             flights = api.get_flights_from_interval(inicio_ts, fin_ts)
-            
+            #resgistra en el log el número de vuelos obtenidos en el bloque o fallo en descarga de bloque 
             if flights is None:
                 logging.warning(f"El bloque {i+1} no devolvió datos, Continuando con el siguiente bloque...")
                 continue
                 
             logging.info(f"Recibidos {len(flights)} vuelos globales. Filtrando por aeropuertos de la UE...")
             
-            # Se construye lista de diccionario de vuelos filtrados por aeropuertos de la UE
+            # Se construye lista de diccionarios de vuelos filtrados por aeropuertos de la UE
             for flight in flights:
                 if flight.estDepartureAirport in eu_aeropuertos:
                     vuelos.append({
