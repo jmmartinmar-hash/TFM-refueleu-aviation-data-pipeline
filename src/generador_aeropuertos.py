3207ab57a8553848538ad4e5ba682224b8aebcab
jmmartinmar <jmmartinmar@gmail.com>
2026-06-23 21:14:32 +0200

    eliminar archivos obsoletos del proyecto

--- src/generador_aeropuertos.py
+++ src/generador_aeropuertos.py
@@ -11,17 +11,17 @@ def genera_master_aeropuertos():
         print(f"❌ Error al descargar los datos: {e}")
         return
     
-    # 1. Filtro por tipo  de aeropuertocomercial (Grandes y Medianos) 
+    # 1. Filtro por tipo  de aeropuerto comercial Grande y Mediano 
     df_filtered = df[df['type'].isin(['large_airport', 'medium_airport'])].copy()
     
     # 2. Limpieza de códigos: Solo códigos ICAO oficiales de 4 letras mayúsculas
     df_filtered = df_filtered[df_filtered['ident'].str.match(r'^[A-Z]{4}$', na=False)]
     
-    # 3. Selección de las columnas necesarioas
+    # 3. Selecono de las columnas necesarias
     columns_requested = ['id', 'ident', 'name', 'continent', 'iso_country', 'municipality']
     df_master = df_filtered[columns_requested].copy()
     
-    # 4.Añado los paises de la UE(27) para  la extracción posterior
+    # 4.Añado columna identificando los aeupuertos de los paises de la UE(27) para la extracción posterior
     eu_countries = [
         'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 
         'FR', 'GR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 
@@ -29,10 +29,10 @@ def genera_master_aeropuertos():
     ]
     df_master['is_ue'] = df_master['iso_country'].isin(eu_countries).astype(int)
     
-    # 5. Eliminar duplicados si los hubiera en la clave primaria (ident)
+    # 5. Elimino duplicados si los hubiera en la clave primaria (ident)
     df_master = df_master.dropna(subset=['ident']).drop_duplicates(subset=['ident'])
     
-    # Guardar el Maestro Global en la raíz del proyecto
+    # 6.Guardo el fichero Maestro Global de aeropuertos 
     output_path = "master_aeropuertos.csv"
     df_master.to_csv(output_path, index=False, encoding="utf-8")
     
